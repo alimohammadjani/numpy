64221130e1795e5a5fcecd9ab110c0a01549ffa1
Ali Mohammadjani <rura565700@gmail.com>
2025-07-21 02:21:47 +0330

    numpy

---
+++ 1.py
@@ -0,0 +1,11 @@
+import numpy as np
+print(np.__version__) #for checking numpy is imported or not
+Z = np.zeros((10,10))
+print("%d bytes" % (Z.size * Z.itemsize))
+I = np.array([0, 1, 2, 3, 15, 16, 32, 64, 128])
+B = ((I.reshape(-1,1) & (2**np.arange(8))) != 0).astype(int)
+print(B[:,::-1])
+
+
+I = np.array([0, 1, 2, 3, 15, 16, 32, 64, 128], dtype=np.uint8)
+print(np.unpackbits(I[:, np.newaxis], axis=1))
